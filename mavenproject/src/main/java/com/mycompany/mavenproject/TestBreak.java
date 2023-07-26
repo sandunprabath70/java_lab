4bbe2082d08fca0c1ce6a6bafad692e245eaf7b4
Sandun Prabath <136352928+sandunprabath70@users.noreply.github.com>
2023-07-26 19:09:35 +0530

    test

---
+++ mavenproject/src/main/java/com/mycompany/mavenproject/TestBreak.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject;
+
+/**
+ *
+ * @author sandunprabath
+ */
+public class Mavenproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
